c9839be801ad16a74ace05ed180484bae0e4c43e
Matt Bacher <mattjb1991@sbcglobal.net>
2018-06-09 12:35:46 -0400

    created cart class with constructor and getter and setter

---
+++ src/main/java/org/javaGeneralStore/maven/Cart.java
@@ -0,0 +1,15 @@
+package org.javaGeneralStore.maven;
+
+public class Cart {
+
+    private final String name;
+
+    public Cart(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
